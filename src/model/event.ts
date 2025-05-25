3e0ca6b6ab179bd325fcb1dce588b525217f98e8
khalifahnur <khalifahnur1095@gmail.com>
2025-05-25 15:32:06 +0300

    feat:added field for logoUrl

--- src/model/event.ts
+++ src/model/event.ts
@@ -2,13 +2,16 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const eventSchema = new Schema({
-  name: { type: String, required: true },
+  homeTeam: { type: String, required: true },
+  awayTeam: { type: String, required: true },
   date: { type: Date, required: true },
   time:{type:String,required:true},
   venue: { type: String, required: true },
   ticketPrice:{type:Number,required: true},
   totalTickets:{type:Number,required:true},
   availableTickets:{type:Number},
+  homeLogoUrl:{type:String},
+  opponentLogoUrl:{type:String},
   createdAt: { type: Date, default: Date.now },
 });
 
