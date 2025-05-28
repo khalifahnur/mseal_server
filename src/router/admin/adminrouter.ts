09c1f6917201d1354b3d79c8d883c1b3a1982729
khalifahnur <khalifahnur1095@gmail.com>
2025-05-28 12:59:19 +0300

    feat:added router for fetching membership info

--- src/router/admin/adminrouter.ts
+++ src/router/admin/adminrouter.ts
@@ -9,6 +9,7 @@ const LogoutAdmin = require("../../controller/auth/admin/logout");
 const StaffSignUp = require("../../controller/auth/admin/staffSignup");
 const FetchAllStaff = require("../../controller/auth/admin/fetchStaff");
 const RemoveStaffAccount = require("../../controller/auth/admin/removeStaff");
+const FetchMembershipInfo = require("../../controller/auth/admin/fetchMemberInfo")
 
 const authenticateAdmin = require("../../middleware/adminMiddleware");
 
@@ -28,5 +29,6 @@ router.post("/logout-admin", authenticateAdmin, LogoutAdmin);
 router.post("/Staff/app-signup", authenticateAdmin, StaffSignUp);
 router.get("/fetch-all-staff-details",authenticateAdmin,FetchAllStaff)
 router.post("/remove-staff-account",authenticateAdmin,RemoveStaffAccount)
+router.get("/fetch-membership-info",authenticateAdmin,FetchMembershipInfo)
 
 module.exports = router;
