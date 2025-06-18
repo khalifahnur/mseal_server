2a58074a6e198321acc1b07f224fcd65e81e0977
khalifahnur <khalifahnur1095@gmail.com>
2025-06-18 19:51:24 +0300

    added pesapal router

--- src/router/payment/paymentrouter.ts
+++ src/router/payment/paymentrouter.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
@@ -10,6 +11,9 @@ const checkPaymentStatus = require("../../controller/payment/membership/verifypa
 const ticketWebhook = require("../../controller/payment/ticket/ticketwebhook");
 const inititateOrderPayment = require("../../controller/payment/order/initiateorderpayment");
 const orderwebhook = require("../../controller/payment/order/orderwebhook");
+
+const initiatePesapalMembershipPayment = require("../../controller/payment/membership/pesapal/initiatepayment");
+const pesapalMembershipPaymentCallback = require("../../controller/payment/membership/pesapal/callback");
 
 router.post("/initiate-payment", authenticateUser, initiateMembershipPayment);
 router.post("/membership-web-hook", handleWebhook);
@@ -24,5 +28,20 @@ router.post("/ticket-webhook", ticketWebhook);
 //orders
 router.post("/initiate-order-payment", authenticateUser, inititateOrderPayment);
 router.post("/order-webhook", orderwebhook);
+
+//pesapal
+//membership
+router.post(
+  "/pesapal/initiate-payment",
+  authenticateUser,
+  [
+    //body("amount").isFloat({ min: 0.01 }).withMessage("Invalid amount"),
+    body("tier").isIn(["bronze", "silver", "gold"]).withMessage("Invalid tier"),
+    body("isUpgrade").isBoolean().withMessage("Invalid upgrade flag"),
+  ],
+  initiatePesapalMembershipPayment
+);
+
+router.post("/pesapal/initiate-membership-callback",pesapalMembershipPaymentCallback);
 
 module.exports = router;
