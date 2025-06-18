42f7c07c59d663d9ba16860c8b2522cc84774f59
khalifahnur <khalifahnur1095@gmail.com>
2025-06-18 19:41:26 +0300

    added paymentStatus field

--- src/model/membership.ts
+++ src/model/membership.ts
@@ -16,21 +16,13 @@ const membershipSchema = new Schema({
     enum: ["valid", "denied"],
     default: "valid",
   },
+  paymentStatus: { type: String, enum: ['pending', 'completed', 'failed'], default: 'pending' },
   dob: { type: String, required: true },
   physicalAddress: { type: String, required: true },
   city: { type: String, required: true },
   reference: { type: String, unique: true },
   createdAt: { type: Date, default: Date.now },
   expDate: { type: Date },
-});
-
-membershipSchema.pre("save", function (next) {
-  if (!this.expDate) {
-    const date = new Date(this.createdAt || Date.now());
-    date.setMonth(date.getMonth() + 1);
-    this.expDate = date;
-  }
-  next();
 });
 
 const Membership = model("Membership", membershipSchema);
